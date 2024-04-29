dad294f6e1128323f06177710e1d3c0d1b46cc93
biti-k <diegoarmandodluca@gmail.com@git config --global user.email diegoarmandodluca@gmail.comgit config --global user.email diegoarmandodluca@gmail.com@>
2024-04-29 20:33:06 +0200

    plural!

--- projecteApp/app/Models/BeaconsModel.php
+++ projecteApp/app/Models/BeaconsModel.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class BeaconModel extends Model
+class BeaconsModel extends Model
 {
     use HasFactory;
 
--- projecteApp/app/Models/CategoriesModel.php
+++ projecteApp/app/Models/CategoriesModel.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class CategoriaModel extends Model
+class CategoriesModel extends Model
 {
     use HasFactory;
     protected $primaryKey = "cat_id";
