e5ac777b7995f11baa8bce0adcb363eacd0560c3
biti-k <diegoarmandodluca@gmail.com@git config --global user.email diegoarmandodluca@gmail.comgit config --global user.email diegoarmandodluca@gmail.com@>
2024-04-29 16:46:20 +0200

    mesas

---
+++ projecteApp/database/migrations/2024_04_29_144320_create_registres_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('registres', function (Blueprint $table) {
+            $table->unsignedBigInteger('reg_id')->autoIncrement();
+            $table->unsignedBigInteger("reg_ins_id");
+            $table->unsignedBigInteger("reg_chk_id");
+            $table->dateTime("reg_temps");
+            $table->foreign('reg_ins_id')->references('ins_id')->on('inscripcio');
+            $table->foreign('reg_chk_id')->references('chk_id')->on('checkpoints');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('registres');
+    }
+};
