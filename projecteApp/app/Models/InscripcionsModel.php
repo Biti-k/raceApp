a33c2fba8a10ee6ec43b6bc2b619b52cb56270c2
biti-k <diegoarmandodluca@gmail.com@git config --global user.email diegoarmandodluca@gmail.comgit config --global user.email diegoarmandodluca@gmail.com@>
2024-04-30 16:01:49 +0200

    inscripcions model

--- projecteApp/app/Models/InscripcionsModel.php
+++ projecteApp/app/Models/InscripcionsModel.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class InscricpionsModel extends Model
+class InscripcionsModel extends Model
 {
     use HasFactory;
 
