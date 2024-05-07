a4c0d0e6542832f26e232a15f01780d4e86ce8f1
biti-k <diegoarmandodluca@gmail.com@git config --global user.email diegoarmandodluca@gmail.comgit config --global user.email diegoarmandodluca@gmail.com@>
2024-05-07 20:17:14 +0200

    dsadasda

---
+++ projecteApp/resources/react/Components/Modal.jsx
@@ -0,0 +1,10 @@
+export const Modal = ({cerrarModal, abrirModal}) => {
+   
+    return (
+        <div className='flex flex-wrap justify-center gap-4'>
+
+        </div>
+    );
+}
+
+export default Modal;
