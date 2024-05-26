bfde2f482ec36e1dd2fba210b36e62328e271628
biti-k <diegoarmandodluca@gmail.com@git config --global user.email diegoarmandodluca@gmail.comgit config --global user.email diegoarmandodluca@gmail.com@>
2024-05-26 14:40:03 +0200

    store registres

--- projecteApp/app/Http/Controllers/RegistresController.php
+++ projecteApp/app/Http/Controllers/RegistresController.php
@@ -14,9 +14,13 @@ class RegistresController extends Controller
     }
     public function store(Request $request)
     {
-        $data = $request->all();
-
-        $registre = RegistresModel::create($data);
+        $registre = RegistresModel::create(
+            [
+                "reg_ins_id" => $request->input("reg_ins_id"),
+                "reg_chk_id" => $request->input('reg_chk_id'),
+                "reg_temps" => $request->input('reg_temps'),
+            ]
+        );
 
         return response()->json(['registre' => RegistresModel::getWithRelations(['reg_id' => $registre->reg_id])]);
     }
